fadea4f9f9d7f9cf5b954ae69cde26d58691b7b2
Ibrahim Quaizar <48260684+ibbydiggy250@users.noreply.github.com>
2026-04-15 00:46:40 -0400

    Update next.config.ts to change output setting

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
+/*
 import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+};
+
+export default nextConfig;
+*/
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
 };
 
 export default nextConfig;
